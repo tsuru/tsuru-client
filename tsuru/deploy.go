14796b4ec2ecb641c514ec41d422e749167e6ecf
Francisco Souza <fss@corp.globo.com>
2014-08-26 17:48:51 -0300

    tsuru: add deploy command
    This command will send the provided files and directories to the tsuru
    server.
    
    Related to tsuru/tsuru#874.

---
+++ tsuru/deploy.go
@@ -0,0 +1,176 @@
+// Copyright 2014 tsuru-client authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/tsuru/tsuru/cmd"
+	"github.com/tsuru/tsuru/cmd/tsuru-base"
+)
+
+type deploy struct {
+	tsuru.GuessingCommand
+}
+
+func (c *deploy) Info() *cmd.Info {
+	desc := `Deploys set of files and/or directories to tsuru server. Some examples of calls are:
+
+tsuru deploy .
+tsuru deploy myfile.jar Procfile
+`
+	return &cmd.Info{
+		Name:    "deploy",
+		Usage:   "deploy [-a/--app <appname>] <file-or-dir-1> [file-or-dir-2] ... [file-or-dir-n]",
+		Desc:    desc,
+		MinArgs: 1,
+	}
+}
+
+func (c *deploy) Run(context *cmd.Context, client *cmd.Client) error {
+	var body bytes.Buffer
+	writer := multipart.NewWriter(&body)
+	file, err := writer.CreateFormFile("file", "archive.tar.gz")
+	if err != nil {
+		return err
+	}
+	err = targz(context, file, context.Args...)
+	if err != nil {
+		return err
+	}
+	writer.Close()
+	appName, err := c.Guess()
+	if err != nil {
+		return err
+	}
+	url, err := cmd.GetURL("/apps/" + appName + "/deploy")
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest("POST", url, &body)
+	if err != nil {
+		return err
+	}
+	request.Header.Set("Content-Type", "multipart/form-data; boundary="+writer.Boundary())
+	resp, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	var respBody bytes.Buffer
+	_, err = io.Copy(io.MultiWriter(&respBody, context.Stdout), resp.Body)
+	if err != nil {
+		return err
+	}
+	if strings.HasSuffix(respBody.String(), "\nOK\n") {
+		return nil
+	}
+	return cmd.ErrAbortCommand
+}
+
+func targz(ctx *cmd.Context, destination io.Writer, filepaths ...string) error {
+	var buf bytes.Buffer
+	tarWriter := tar.NewWriter(&buf)
+	for _, path := range filepaths {
+		if path == ".." {
+			fmt.Fprintf(ctx.Stderr, "Warning: skipping %q", path)
+			continue
+		}
+		fi, err := os.Stat(path)
+		if err != nil {
+			return err
+		}
+		if fi.IsDir() {
+			err = addDir(tarWriter, path)
+		} else {
+			err = addFile(tarWriter, path)
+		}
+		if err != nil {
+			return err
+		}
+	}
+	err := tarWriter.Close()
+	if err != nil {
+		return err
+	}
+	gzipWriter := gzip.NewWriter(destination)
+	defer gzipWriter.Close()
+	_, err = io.Copy(gzipWriter, &buf)
+	return err
+}
+
+func addDir(writer *tar.Writer, path string) error {
+	dir, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer dir.Close()
+	fi, err := dir.Stat()
+	if err != nil {
+		return err
+	}
+	header, err := tar.FileInfoHeader(fi, "")
+	if err != nil {
+		return err
+	}
+	header.Name = path
+	err = writer.WriteHeader(header)
+	if err != nil {
+		return err
+	}
+	fis, err := dir.Readdir(0)
+	if err != nil {
+		return err
+	}
+	for _, fi := range fis {
+		if fi.IsDir() {
+			err = addDir(writer, filepath.Join(path, fi.Name()))
+		} else {
+			err = addFile(writer, filepath.Join(path, fi.Name()))
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func addFile(writer *tar.Writer, path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		return err
+	}
+	header, err := tar.FileInfoHeader(fi, "")
+	if err != nil {
+		return err
+	}
+	header.Name = path
+	err = writer.WriteHeader(header)
+	if err != nil {
+		return err
+	}
+	n, err := io.Copy(writer, f)
+	if err != nil {
+		return err
+	}
+	if n != fi.Size() {
+		return io.ErrShortWrite
+	}
+	return nil
+}
---
+++ tsuru/deploy_test.go
@@ -0,0 +1,155 @@
+// Copyright 2014 tsuru-client authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/tsuru/tsuru/cmd"
+	"github.com/tsuru/tsuru/cmd/testing"
+	"github.com/tsuru/tsuru/cmd/tsuru-base"
+	"launchpad.net/gocheck"
+)
+
+func (s *S) TestDeployInfo(c *gocheck.C) {
+	desc := `Deploys set of files and/or directories to tsuru server. Some examples of calls are:
+
+tsuru deploy .
+tsuru deploy myfile.jar Procfile
+`
+	expected := &cmd.Info{
+		Name:    "deploy",
+		Usage:   "deploy [-a/--app <appname>] <file-or-dir-1> [file-or-dir-2] ... [file-or-dir-n]",
+		Desc:    desc,
+		MinArgs: 1,
+	}
+	cmd := deploy{}
+	c.Assert(cmd.Info(), gocheck.DeepEquals, expected)
+}
+
+func (s *S) TestDeployRun(c *gocheck.C) {
+	var called bool
+	var buf bytes.Buffer
+	err := targz(nil, &buf, "testdata")
+	c.Assert(err, gocheck.IsNil)
+	trans := testing.ConditionalTransport{
+		Transport: testing.Transport{Message: "deploy worked\nOK\n", Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			defer req.Body.Close()
+			called = true
+			file, _, err := req.FormFile("file")
+			c.Assert(err, gocheck.IsNil)
+			content, err := ioutil.ReadAll(file)
+			c.Assert(err, gocheck.IsNil)
+			c.Assert(content, gocheck.DeepEquals, buf.Bytes())
+			return req.Method == "POST" && req.URL.Path == "/apps/secret/deploy"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: &trans}, nil, manager)
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Stdout: &stdout,
+		Stderr: &stderr,
+		Args:   []string{"testdata", ".."},
+	}
+	fake := FakeGuesser{name: "secret"}
+	guessCommand := tsuru.GuessingCommand{G: &fake}
+	cmd := deploy{GuessingCommand: guessCommand}
+	err = cmd.Run(&context, client)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(called, gocheck.Equals, true)
+}
+
+func (s *S) TestDeployRunNotOK(c *gocheck.C) {
+	trans := testing.Transport{Message: "deploy worked\n", Status: http.StatusOK}
+	client := cmd.NewClient(&http.Client{Transport: &trans}, nil, manager)
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Stdout: &stdout,
+		Stderr: &stderr,
+		Args:   []string{"testdata", ".."},
+	}
+	fake := FakeGuesser{name: "secret"}
+	guessCommand := tsuru.GuessingCommand{G: &fake}
+	command := deploy{GuessingCommand: guessCommand}
+	err := command.Run(&context, client)
+	c.Assert(err, gocheck.Equals, cmd.ErrAbortCommand)
+}
+
+func (s *S) TestDeployRunFileNotFound(c *gocheck.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Stdout: &stdout,
+		Stderr: &stderr,
+		Args:   []string{"/tmp/something/that/doesnt/really/exist/im/sure"},
+	}
+	fake := FakeGuesser{name: "secret"}
+	guessCommand := tsuru.GuessingCommand{G: &fake}
+	command := deploy{GuessingCommand: guessCommand}
+	err := command.Run(&context, nil)
+	c.Assert(err, gocheck.NotNil)
+}
+
+func (s *S) TestDeployRunRequestFailure(c *gocheck.C) {
+	trans := testing.Transport{Message: "app not found\n", Status: http.StatusNotFound}
+	client := cmd.NewClient(&http.Client{Transport: &trans}, nil, manager)
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Stdout: &stdout,
+		Stderr: &stderr,
+		Args:   []string{"testdata", ".."},
+	}
+	fake := FakeGuesser{name: "secret"}
+	guessCommand := tsuru.GuessingCommand{G: &fake}
+	command := deploy{GuessingCommand: guessCommand}
+	err := command.Run(&context, client)
+	c.Assert(err, gocheck.NotNil)
+	c.Assert(err.Error(), gocheck.Equals, "app not found\n")
+}
+
+func (s *S) TestTargz(c *gocheck.C) {
+	var buf bytes.Buffer
+	ctx := cmd.Context{Stderr: &buf}
+	var gzipBuf, tarBuf bytes.Buffer
+	err := targz(&ctx, &gzipBuf, "testdata", "..")
+	c.Assert(err, gocheck.IsNil)
+	gzipReader, err := gzip.NewReader(&gzipBuf)
+	c.Assert(err, gocheck.IsNil)
+	_, err = io.Copy(&tarBuf, gzipReader)
+	c.Assert(err, gocheck.IsNil)
+	tarReader := tar.NewReader(&tarBuf)
+	var headers []string
+	var contents []string
+	for header, err := tarReader.Next(); err == nil; header, err = tarReader.Next() {
+		headers = append(headers, header.Name)
+		if !header.FileInfo().IsDir() {
+			content, err := ioutil.ReadAll(tarReader)
+			c.Assert(err, gocheck.IsNil)
+			contents = append(contents, string(content))
+		}
+	}
+	expected := []string{
+		"testdata", "testdata/directory", "testdata/directory/file.txt",
+		"testdata/file1.txt", "testdata/file2.txt",
+	}
+	c.Assert(headers, gocheck.DeepEquals, expected)
+	expectedContents := []string{"wat\n", "something happened\n", "twice\n"}
+	c.Assert(contents, gocheck.DeepEquals, expectedContents)
+	c.Assert(buf.String(), gocheck.Equals, `Warning: skipping ".."`)
+}
+
+func (s *S) TestTargzFailure(c *gocheck.C) {
+	var stderr bytes.Buffer
+	ctx := cmd.Context{Stderr: &stderr}
+	var buf bytes.Buffer
+	err := targz(&ctx, &buf, "/tmp/something/that/definitely/doesnt/exist/right", "testdata")
+	c.Assert(err, gocheck.NotNil)
+	c.Assert(err.Error(), gocheck.Equals, "stat /tmp/something/that/definitely/doesnt/exist/right: no such file or directory")
+}
