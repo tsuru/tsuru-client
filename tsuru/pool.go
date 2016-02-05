0ab6454f8eafd51a7c8cde4c95a242df78f97a94
Cezar Sa Espinola <cezarsa@gmail.com>
2016-02-05 19:37:48 -0200

    fix license header and package declaration

--- tsuru/pool.go
+++ tsuru/pool.go
@@ -1,6 +1,8 @@
 // Copyright 2016 tsuru-client authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.package main
+// license that can be found in the LICENSE file.
+
+package main
 
 import (
 	"encoding/json"
