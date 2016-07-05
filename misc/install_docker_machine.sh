b5927bd42c5b6f0e0ba34577a097f627c37089a8
André Carvalho <andre.carvalho@corp.globo.com>
2016-07-05 12:59:55 -0300

    travis: install docker machine with sudo

--- misc/install_docker_machine.sh
+++ misc/install_docker_machine.sh
@@ -4,5 +4,5 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-curl -L https://github.com/docker/machine/releases/download/v0.8.0-rc1/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
+sudo curl -L https://github.com/docker/machine/releases/download/v0.8.0-rc1/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
 chmod +x /usr/local/bin/docker-machine
