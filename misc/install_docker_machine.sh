4d4a5cf1e765c7534e1be64ea3cbf4dc33938705
André Carvalho <andre.carvalho@corp.globo.com>
2016-07-05 12:39:41 -0300

    travis: install docker-machine

---
+++ misc/install_docker_machine.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+# Copyright 2016 tsuru authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+curl -L https://github.com/docker/machine/releases/download/v0.8.0-rc1/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
+chmod +x /usr/local/bin/docker-machine
