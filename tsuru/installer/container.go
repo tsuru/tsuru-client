2d32f41f36f8c79e47a9f5518afec47dcab8c2df
André Carvalho <andre.carvalho@corp.globo.com>
2016-08-30 18:40:53 -0300

    installer: adds struct to manage swarm clusters

--- tsuru/installer/container.go
+++ tsuru/installer/container.go
@@ -5,6 +5,7 @@
 package installer
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -14,9 +15,141 @@ import (
 	"github.com/fsouza/go-dockerclient"
 )
 
+var swarmPort = 2377
+
 type dockerEnpoint interface {
 	dockerClient() (*docker.Client, error)
 	GetNetwork() *docker.Network
+}
+
+type SwarmCluster struct {
+	Manager *Machine
+	Workers []*Machine
+}
+
+func (c *SwarmCluster) dockerClient() (*docker.Client, error) {
+	return c.Manager.dockerClient()
+}
+
+func (c *SwarmCluster) GetNetwork() *docker.Network {
+	return c.Manager.GetNetwork()
+}
+
+// NewSwarmCluster creates a Swarm Cluster using the first machine as a manager
+// and the rest as workers and also creates an overlay network between the nodes.
+func NewSwarmCluster(machines []*Machine) (*SwarmCluster, error) {
+	swarmOpts := docker.InitSwarmOptions{
+		InitRequest: swarm.InitRequest{
+			ListenAddr:    fmt.Sprintf("0.0.0.0:%d", swarmPort),
+			AdvertiseAddr: fmt.Sprintf("%s:%d", machines[0].IP, swarmPort),
+		},
+	}
+	dockerClient, err := machines[0].dockerClient()
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve machine %s docker client: %s", machines[0].Name, err)
+	}
+	_, err = dockerClient.InitSwarm(swarmOpts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to init swarm: %s", err)
+	}
+	swarmInspect, err := dockerClient.InspectSwarm(nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to inspect swarm: %s", err)
+	}
+	createNetworkOpts := docker.CreateNetworkOptions{
+		Name:           "tsuru",
+		Driver:         "overlay",
+		CheckDuplicate: true,
+		IPAM: docker.IPAMOptions{
+			Driver: "default",
+			Config: []docker.IPAMConfig{
+				{
+					Subnet: "10.0.9.0/24",
+				},
+			},
+		},
+	}
+	network, err := dockerClient.CreateNetwork(createNetworkOpts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create overlay network: %s", err)
+	}
+	for i, m := range machines {
+		m.network = network
+		if i == 0 {
+			continue
+		}
+		dockerClient, err = m.dockerClient()
+		if err != nil {
+			return nil, fmt.Errorf("failed to retrieve machine %s docker client: %s", m.Name, err)
+		}
+		opts := docker.JoinSwarmOptions{
+			JoinRequest: swarm.JoinRequest{
+				ListenAddr:    fmt.Sprintf("0.0.0.0:%d", swarmPort),
+				AdvertiseAddr: fmt.Sprintf("%s:%d", m.IP, swarmPort),
+				JoinToken:     swarmInspect.JoinTokens.Worker,
+				RemoteAddrs:   []string{fmt.Sprintf("%s:%d", machines[0].IP, swarmPort)},
+			},
+		}
+		err = dockerClient.JoinSwarm(opts)
+		if err != nil {
+			return nil, fmt.Errorf("machine %s failed to join swarm: %s", m.Name, err)
+		}
+	}
+	return &SwarmCluster{
+		Manager: machines[0],
+		Workers: machines,
+	}, nil
+}
+
+// ServiceTaskExec finds a container running a service task and runs exec on it
+func (c *SwarmCluster) ServiceExec(service string, cmd []string, startOpts docker.StartExecOptions) error {
+	mClient, err := c.dockerClient()
+	if err != nil {
+		return fmt.Errorf("failed to retrive swarm docker client: %s", err)
+	}
+	tasks, err := mClient.ListTasks(docker.ListTasksOptions{
+		Filters: map[string][]string{
+			"service":       {service},
+			"desired-state": {"running"},
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("failed to list tasks for service %s: %s", service, err)
+	}
+	if len(tasks) == 0 {
+		return fmt.Errorf("no running task found for service %s", service)
+	}
+	node, err := mClient.InspectNode(tasks[0].NodeID)
+	if err != nil {
+		return fmt.Errorf("failed to inspect node %s: %s", tasks[0].NodeID, err)
+	}
+	nodeName := node.Description.Hostname
+	var machine *Machine
+	for _, m := range c.Workers {
+		if m.Name == nodeName {
+			machine = m
+			break
+		}
+	}
+	if machine == nil {
+		return fmt.Errorf("machine %s not found", nodeName)
+	}
+	client, err := machine.dockerClient()
+	if err != nil {
+		return fmt.Errorf("failed to retrieve task node %s docker client: %s", machine.Name, err)
+	}
+	container := tasks[0].Status.ContainerStatus.ContainerID
+	exec, err := client.CreateExec(docker.CreateExecOptions{
+		Cmd:          cmd,
+		AttachStdout: true,
+		AttachStderr: true,
+		AttachStdin:  true,
+		Container:    container,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to exec in task container %s: %s", container, err)
+	}
+	return client.StartExec(exec.ID, startOpts)
 }
 
 func createContainer(d dockerEnpoint, name string, config *docker.Config, hostConfig *docker.HostConfig) error {
@@ -103,4 +236,20 @@ func createContainer(d dockerEnpoint, name string, config *docker.Config, hostCo
 	}
 	_, err = client.CreateService(serviceCreateOpts)
 	return err
+}
+
+func (c *SwarmCluster) ListNodes() ([]swarm.Node, error) {
+	client, err := c.dockerClient()
+	if err != nil {
+		return nil, err
+	}
+	return client.ListNodes(docker.ListNodesOptions{})
+}
+
+func (c *SwarmCluster) ListServices() ([]swarm.Service, error) {
+	client, err := c.dockerClient()
+	if err != nil {
+		return nil, err
+	}
+	return client.ListServices(docker.ListServicesOptions{})
 }
--- tsuru/installer/container_test.go
+++ tsuru/installer/container_test.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"github.com/docker/engine-api/types/swarm"
+	"github.com/docker/machine/libmachine/host"
 	docker "github.com/fsouza/go-dockerclient"
 	"github.com/fsouza/go-dockerclient/testing"
 	"gopkg.in/check.v1"
@@ -141,4 +143,149 @@ func (s *S) TestCreateContainerExposedPortsNotOverridesHostConfig(c *check.C) {
 	cont := <-containerChan
 	c.Assert(cont, check.NotNil)
 	c.Assert(expected, check.DeepEquals, cont.HostConfig.PortBindings)
+}
+
+func (s *S) TestNewSwarmCluster(c *check.C) {
+	tlsConfig := testing.TLSConfig{
+		CertPath:    s.TLSCertsPath.ServerCert,
+		CertKeyPath: s.TLSCertsPath.ServerKey,
+		RootCAPath:  s.TLSCertsPath.RootCert,
+	}
+	var managerReqs, workerReqs []*http.Request
+	managerServer, err := testing.NewTLSServer("127.0.0.1:0", nil, func(r *http.Request) {
+		managerReqs = append(managerReqs, r)
+		if r.URL.Path == "/swarm/init" {
+			var initReq swarm.InitRequest
+			errDec := json.NewDecoder(r.Body).Decode(&initReq)
+			c.Assert(errDec, check.IsNil)
+			c.Assert(initReq.AdvertiseAddr, check.Equals, "127.0.0.1:2377")
+			c.Assert(initReq.ListenAddr, check.Equals, "0.0.0.0:2377")
+		}
+	}, tlsConfig)
+	c.Assert(err, check.IsNil)
+	defer managerServer.Stop()
+	managerMachine := &Machine{
+		Host:    &host.Host{Name: "manager"},
+		IP:      "127.0.0.1",
+		Address: managerServer.URL(),
+		CAPath:  s.TLSCertsPath.RootDir,
+		network: &docker.Network{Name: "tsuru"},
+	}
+	workerServer, err := testing.NewTLSServer("127.0.0.1:0", nil, func(r *http.Request) {
+		workerReqs = append(workerReqs, r)
+		if r.URL.Path == "/swarm/join" {
+			var joinReq swarm.JoinRequest
+			errDec := json.NewDecoder(workerReqs[0].Body).Decode(&joinReq)
+			c.Assert(errDec, check.IsNil)
+			c.Assert(joinReq.RemoteAddrs, check.DeepEquals, []string{"127.0.0.1:2377"})
+			c.Assert(joinReq.AdvertiseAddr, check.Equals, "127.0.0.2:2377")
+			c.Assert(joinReq.ListenAddr, check.Equals, "0.0.0.0:2377")
+		}
+	}, tlsConfig)
+	c.Assert(err, check.IsNil)
+	defer workerServer.Stop()
+	workerMachine := &Machine{
+		Host:    &host.Host{Name: "worker"},
+		IP:      "127.0.0.2",
+		Address: workerServer.URL(),
+		CAPath:  s.TLSCertsPath.RootDir,
+		network: &docker.Network{Name: "tsuru"},
+	}
+	cluster, err := NewSwarmCluster([]*Machine{managerMachine, workerMachine})
+	c.Assert(err, check.IsNil)
+	c.Assert(cluster, check.NotNil)
+	c.Assert(managerReqs[0].URL.Path, check.Equals, "/swarm/init")
+	c.Assert(managerReqs[1].URL.Path, check.Equals, "/swarm")
+	c.Assert(managerReqs[2].URL.Path, check.Equals, "/networks/create")
+	c.Assert(workerReqs[0].URL.Path, check.Equals, "/swarm/join")
+}
+
+func (s *S) TestServiceExec(c *check.C) {
+	tlsConfig := testing.TLSConfig{
+		CertPath:    s.TLSCertsPath.ServerCert,
+		CertKeyPath: s.TLSCertsPath.ServerKey,
+		RootCAPath:  s.TLSCertsPath.RootCert,
+	}
+	execStarted := false
+	managerServer, err := testing.NewTLSServer("127.0.0.1:0", nil, nil, tlsConfig)
+	c.Assert(err, check.IsNil)
+	defer managerServer.Stop()
+	workerServer, err := testing.NewTLSServer("127.0.0.1:0", nil, func(r *http.Request) {
+		if r.URL.Path == "/exec/exec-ID/start" {
+			execStarted = true
+		}
+	}, tlsConfig)
+	c.Assert(err, check.IsNil)
+	defer workerServer.Stop()
+	managerMachine := &Machine{
+		Host:    &host.Host{Name: "manager"},
+		IP:      "127.0.0.1",
+		Address: managerServer.URL(),
+		CAPath:  s.TLSCertsPath.RootDir,
+		network: &docker.Network{Name: "tsuru"},
+	}
+	workerMachine := &Machine{
+		Host:    &host.Host{Name: "worker"},
+		IP:      "127.0.0.1",
+		Address: workerServer.URL(),
+		CAPath:  s.TLSCertsPath.RootDir,
+		network: &docker.Network{Name: "tsuru"},
+	}
+	cluster := &SwarmCluster{
+		Manager: managerMachine,
+		Workers: []*Machine{managerMachine, workerMachine},
+	}
+	err = createContainer(cluster, "tsuru", &docker.Config{Image: "tsuru/tsuru"}, &docker.HostConfig{})
+	c.Assert(err, check.IsNil)
+	client, err := cluster.Workers[0].dockerClient()
+	c.Assert(err, check.IsNil)
+	cont, err := client.CreateContainer(docker.CreateContainerOptions{
+		Config: &docker.Config{Image: "tsuru/tsuru"},
+	})
+	c.Assert(err, check.IsNil)
+	managerServer.CustomHandler("/tasks", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var filters map[string][]string
+		errJSON := json.Unmarshal([]byte(r.URL.Query().Get("filters")), &filters)
+		c.Assert(errJSON, check.IsNil)
+		c.Assert(filters["service"], check.DeepEquals, []string{"tsuru"})
+		c.Assert(filters["desired-state"], check.DeepEquals, []string{"running"})
+		tasks := []swarm.Task{
+			{
+				ID:     "123",
+				NodeID: "node-123",
+				Status: swarm.TaskStatus{
+					ContainerStatus: swarm.ContainerStatus{
+						ContainerID: cont.ID},
+				},
+			},
+		}
+		buf, errMarshal := json.Marshal(tasks)
+		c.Assert(errMarshal, check.IsNil)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(buf)
+		w.WriteHeader(http.StatusOK)
+	}))
+	managerServer.CustomHandler("/nodes/node-123", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		node := swarm.Node{
+			Description: swarm.NodeDescription{
+				Hostname: "worker",
+			},
+		}
+		buf, errMarshal := json.Marshal(node)
+		c.Assert(errMarshal, check.IsNil)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(buf)
+		w.WriteHeader(http.StatusOK)
+	}))
+	workerServer.CustomHandler("/containers/"+cont.ID+"/exec", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var opts docker.CreateExecOptions
+		errJSON := json.NewDecoder(r.Body).Decode(&opts)
+		c.Assert(errJSON, check.IsNil)
+		c.Assert(opts.Cmd, check.DeepEquals, []string{"exit"})
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{"Id": "exec-ID"})
+	}))
+	err = cluster.ServiceExec("tsuru", []string{"exit"}, docker.StartExecOptions{})
+	c.Assert(err, check.IsNil)
+	c.Assert(execStarted, check.Equals, true)
 }
