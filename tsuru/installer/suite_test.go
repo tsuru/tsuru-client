44e3db781205d7437fe2f414bdcc9c08cf44ec7d
André Carvalho <andre.carvalho@corp.globo.com>
2016-09-27 09:30:07 -0300

    installer: fix tests that depends on dm plugins

--- tsuru/installer/suite_test.go
+++ tsuru/installer/suite_test.go
@@ -6,9 +6,12 @@ package installer
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"testing"
 
+	"github.com/docker/machine/libmachine/drivers/plugin/localbinary"
+	"github.com/tsuru/tsuru-client/tsuru/installer/dm"
 	"github.com/tsuru/tsuru-client/tsuru/installer/testing"
 	"github.com/tsuru/tsuru/cmd"
 	check "gopkg.in/check.v1"
@@ -30,4 +33,18 @@ func (s *S) SetUpSuite(c *check.C) {
 	s.TLSCertsPath = tlsCertsPath
 	var stdout, stderr bytes.Buffer
 	manager = cmd.NewManager("glb", "1.0.0", "Supported-Tsuru-Version", &stdout, &stderr, os.Stdin, nil)
+}
+
+func TestMain(m *testing.M) {
+	if os.Getenv(localbinary.PluginEnvKey) == localbinary.PluginEnvVal {
+		driver := os.Getenv(localbinary.PluginEnvDriverName)
+		err := dm.RunDriver(driver)
+		if err != nil {
+			fmt.Printf("Failed to run driver %s in test", driver)
+			os.Exit(1)
+		}
+	} else {
+		localbinary.CurrentBinaryIsDockerMachine = true
+		os.Exit(m.Run())
+	}
 }
