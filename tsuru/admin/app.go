e1a9e366c2d1c0e02c6bb9e87763bc6bf09145f5
André Carvalho <asantostc@gmail.com>
2016-11-01 09:57:16 -0200

    admin: migrate app cmds from tsuru-admin

---
+++ tsuru/admin/app.go
@@ -0,0 +1,124 @@
+// Copyright 2016 tsuru-client authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package admin
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/tsuru/gnuflag"
+	"github.com/tsuru/tsuru/cmd"
+	"github.com/tsuru/tsuru/router/rebuild"
+)
+
+type AppLockDelete struct {
+	cmd.GuessingCommand
+	cmd.ConfirmationCommand
+	fs *gnuflag.FlagSet
+}
+
+func (c *AppLockDelete) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "app-unlock",
+		MinArgs: 0,
+		Usage:   "app-unlock -a <app-name> [-y]",
+		Desc: `Forces the removal of an application lock.
+Use with caution, removing an active lock may cause inconsistencies.`,
+	}
+}
+
+func (c *AppLockDelete) Run(ctx *cmd.Context, client *cmd.Client) error {
+	appName, err := c.Guess()
+	if err != nil {
+		return err
+	}
+	if !c.Confirm(ctx, fmt.Sprintf(`Are you sure you want to remove the lock from app "%s"?`, appName)) {
+		return nil
+	}
+	url, err := cmd.GetURL("/apps/" + appName + "/lock")
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return err
+	}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(ctx.Stdout, "Lock successfully removed!\n")
+	return nil
+}
+
+func (c *AppLockDelete) Flags() *gnuflag.FlagSet {
+	if c.fs == nil {
+		c.fs = cmd.MergeFlagSet(
+			c.GuessingCommand.Flags(),
+			c.ConfirmationCommand.Flags(),
+		)
+	}
+	return c.fs
+}
+
+type AppRoutesRebuild struct {
+	cmd.GuessingCommand
+}
+
+func (c *AppRoutesRebuild) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "app-routes-rebuild",
+		MinArgs: 0,
+		Usage:   "app-routes-rebuild -a <app-name>",
+		Desc: `Rebuild routes for an application.
+This can be used to recover from some failure in the router that caused
+existing routes to be lost.`,
+	}
+}
+
+func (c *AppRoutesRebuild) Run(ctx *cmd.Context, client *cmd.Client) error {
+	appName, err := c.Guess()
+	if err != nil {
+		return err
+	}
+	url, err := cmd.GetURL("/apps/" + appName + "/routes")
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return err
+	}
+	rsp, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer rsp.Body.Close()
+	var rebuildResult rebuild.RebuildRoutesResult
+	err = json.NewDecoder(rsp.Body).Decode(&rebuildResult)
+	if err != nil {
+		return err
+	}
+	rebuilt := len(rebuildResult.Added) > 0 || len(rebuildResult.Removed) > 0
+	if len(rebuildResult.Added) > 0 {
+		fmt.Fprintf(ctx.Stdout, "Added routes:\n")
+		for _, added := range rebuildResult.Added {
+			fmt.Fprintf(ctx.Stdout, "- %s\n", added)
+		}
+	}
+	if len(rebuildResult.Removed) > 0 {
+		fmt.Fprintf(ctx.Stdout, "Removed routes:\n")
+		for _, removed := range rebuildResult.Removed {
+			fmt.Fprintf(ctx.Stdout, "- %s\n", removed)
+		}
+	}
+	if rebuilt {
+		fmt.Fprintf(ctx.Stdout, "\nRoutes successfully rebuilt!\n")
+	} else {
+		fmt.Fprintf(ctx.Stdout, "Nothing to do, routes already correct.\n")
+	}
+	return nil
+}
---
+++ tsuru/admin/app_test.go
@@ -0,0 +1,108 @@
+// Copyright 2016 tsuru-client authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package admin
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/tsuru/tsuru/cmd"
+	"github.com/tsuru/tsuru/cmd/cmdtest"
+	"github.com/tsuru/tsuru/router/rebuild"
+	"gopkg.in/check.v1"
+)
+
+func (s *S) TestAppLockDeleteRun(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	expected := "Lock successfully removed!\n"
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			return strings.HasSuffix(req.URL.Path, "/apps/app1/lock") && req.Method == "DELETE"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := AppLockDelete{}
+	command.Flags().Parse(true, []string{"--app", "app1", "-y"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, expected)
+}
+
+func (s *S) TestAppLockDeleteRunAsksConfirmation(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Stdout: &stdout,
+		Stderr: &stderr,
+		Stdin:  strings.NewReader("n\n"),
+	}
+	command := AppLockDelete{}
+	command.Flags().Parse(true, []string{"--app", "app1"})
+	err := command.Run(&context, nil)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, "Are you sure you want to remove the lock from app \"app1\"? (y/n) Abort.\n")
+}
+
+func (s *S) TestAppRoutesRebuildRun(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	rebuildResult := rebuild.RebuildRoutesResult{
+		Added:   []string{"r1", "r2"},
+		Removed: []string{"r9"},
+	}
+	data, err := json.Marshal(rebuildResult)
+	c.Assert(err, check.IsNil)
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: string(data), Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			return strings.HasSuffix(req.URL.Path, "/apps/app1/routes") && req.Method == "POST"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := AppRoutesRebuild{}
+	command.Flags().Parse(true, []string{"--app", "app1"})
+	err = command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, `Added routes:
+- r1
+- r2
+Removed routes:
+- r9
+
+Routes successfully rebuilt!
+`)
+}
+
+func (s *S) TestAppRoutesRebuildRunNothingToDo(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	rebuildResult := rebuild.RebuildRoutesResult{}
+	data, err := json.Marshal(rebuildResult)
+	c.Assert(err, check.IsNil)
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: string(data), Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			return strings.HasSuffix(req.URL.Path, "/apps/app1/routes") && req.Method == "POST"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := AppRoutesRebuild{}
+	command.Flags().Parse(true, []string{"--app", "app1"})
+	err = command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, "Nothing to do, routes already correct.\n")
+}
