14766d9d50d384563eebcdff6c7ac966b225e0df
André Carvalho <asantostc@gmail.com>
2016-11-02 11:58:13 -0200

    admin: migrate plan cmds from tsuru-admin

---
+++ tsuru/admin/plan.go
@@ -0,0 +1,162 @@
+// Copyright 2016 tsuru-client authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package admin
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/tsuru/gnuflag"
+	"github.com/tsuru/tsuru/cmd"
+	"github.com/tsuru/tsuru/router"
+)
+
+type PlanCreate struct {
+	memory     string
+	swap       string
+	cpushare   int
+	setDefault bool
+	router     string
+	fs         *gnuflag.FlagSet
+}
+
+func (c *PlanCreate) Flags() *gnuflag.FlagSet {
+	if c.fs == nil {
+		c.fs = gnuflag.NewFlagSet("plan-create", gnuflag.ExitOnError)
+		memory := `Amount of available memory for units in bytes or an integer value followed
+by M, K or G for megabytes, kilobytes or gigabytes respectively.`
+		c.fs.StringVar(&c.memory, "memory", "0", memory)
+		c.fs.StringVar(&c.memory, "m", "0", memory)
+		swap := `Amount of available swap space for units in bytes or an integer value followed
+by M, K or G for megabytes, kilobytes or gigabytes respectively.`
+		c.fs.StringVar(&c.swap, "swap", "0", swap)
+		c.fs.StringVar(&c.swap, "s", "0", swap)
+		cpushare := `Relative cpu share each unit will have available. This value is unitless and
+relative, so specifying the same value for all plans means all units will
+equally share processing power.`
+		c.fs.IntVar(&c.cpushare, "cpushare", 0, cpushare)
+		c.fs.IntVar(&c.cpushare, "c", 0, cpushare)
+		setDefault := `Set plan as default, this will remove the default flag from any other plan.
+The default plan will be used when creating an application without explicitly
+setting a plan.`
+		c.fs.BoolVar(&c.setDefault, "default", false, setDefault)
+		c.fs.BoolVar(&c.setDefault, "d", false, setDefault)
+		router := "The name of the router used by this plan."
+		c.fs.StringVar(&c.router, "router", "", router)
+		c.fs.StringVar(&c.router, "r", "", router)
+	}
+	return c.fs
+}
+
+func (c *PlanCreate) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "plan-create",
+		Usage:   "plan-create <name> -c cpushare [-m memory] [-s swap] [-r router] [--default]",
+		Desc:    `Creates a new plan for being used when creating apps.`,
+		MinArgs: 1,
+	}
+}
+
+func (c *PlanCreate) Run(context *cmd.Context, client *cmd.Client) error {
+	u, err := cmd.GetURL("/plans")
+	if err != nil {
+		return err
+	}
+	v := url.Values{}
+	v.Set("name", context.Args[0])
+	v.Set("memory", c.memory)
+	v.Set("swap", c.swap)
+	v.Set("cpushare", strconv.Itoa(c.cpushare))
+	v.Set("default", strconv.FormatBool(c.setDefault))
+	v.Set("router", c.router)
+	b := strings.NewReader(v.Encode())
+	request, err := http.NewRequest("POST", u, b)
+	if err != nil {
+		return err
+	}
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	_, err = client.Do(request)
+	if err != nil {
+		fmt.Fprintf(context.Stdout, "Failed to create plan!\n")
+		return err
+	}
+	fmt.Fprintf(context.Stdout, "Plan successfully created!\n")
+	return nil
+}
+
+type PlanRemove struct{}
+
+func (c *PlanRemove) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:  "plan-remove",
+		Usage: "plan-remove <name>",
+		Desc: `Removes an existing plan. It will no longer be available for newly created
+apps. However, this won't change anything for existing apps that were created
+using the removed plan. They will keep using the same value amount of
+resources described by the plan.`,
+		MinArgs: 1,
+	}
+}
+
+func (c *PlanRemove) Run(context *cmd.Context, client *cmd.Client) error {
+	url, err := cmd.GetURL("/plans/" + context.Args[0])
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return err
+	}
+	_, err = client.Do(request)
+	if err != nil {
+		fmt.Fprintf(context.Stdout, "Failed to remove plan!\n")
+		return err
+	}
+	fmt.Fprintf(context.Stdout, "Plan successfully removed!\n")
+	return nil
+}
+
+type PlanRoutersList struct{}
+
+func (c *PlanRoutersList) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "router-list",
+		Usage:   "router-list",
+		Desc:    "List all routers available for plan creation.",
+		MinArgs: 0,
+	}
+}
+
+func (c *PlanRoutersList) Run(context *cmd.Context, client *cmd.Client) error {
+	url, err := cmd.GetURL("/plans/routers")
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	var routers []router.PlanRouter
+	err = json.NewDecoder(response.Body).Decode(&routers)
+	if err != nil {
+		return err
+	}
+	table := cmd.NewTable()
+	table.Headers = cmd.Row([]string{"Name", "Type"})
+	table.LineSeparator = true
+	for _, router := range routers {
+		table.AddRow(cmd.Row([]string{router.Name, router.Type}))
+	}
+	context.Stdout.Write(table.Bytes())
+	return nil
+}
---
+++ tsuru/admin/plan_test.go
@@ -0,0 +1,260 @@
+// Copyright 2016 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package admin
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/tsuru/tsuru/cmd"
+	"github.com/tsuru/tsuru/cmd/cmdtest"
+	"github.com/tsuru/tsuru/router"
+	"gopkg.in/check.v1"
+)
+
+func (s *S) TestPlanCreate(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"myplan"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusCreated},
+		CondFunc: func(req *http.Request) bool {
+			name := req.FormValue("name") == "myplan"
+			memory := req.FormValue("memory") == "0"
+			swap := req.FormValue("swap") == "0"
+			cpuShare := req.FormValue("cpushare") == "100"
+			deflt := req.FormValue("default") == "false"
+			router := req.FormValue("router") == ""
+			method := req.Method == "POST"
+			contentType := req.Header.Get("Content-Type") == "application/x-www-form-urlencoded"
+			url := strings.HasSuffix(req.URL.Path, "/plans")
+			return method && url && contentType && name && memory && swap && cpuShare && deflt && router
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := PlanCreate{}
+	command.Flags().Parse(true, []string{"-c", "100"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, "Plan successfully created!\n")
+}
+
+func (s *S) TestPlanCreateFlags(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"myplan"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusCreated},
+		CondFunc: func(req *http.Request) bool {
+			name := req.FormValue("name") == "myplan"
+			memory := req.FormValue("memory") == "4194304"
+			swap := req.FormValue("swap") == "512"
+			cpuShare := req.FormValue("cpushare") == "100"
+			deflt := req.FormValue("default") == "true"
+			router := req.FormValue("router") == "myrouter"
+			method := req.Method == "POST"
+			contentType := req.Header.Get("Content-Type") == "application/x-www-form-urlencoded"
+			url := strings.HasSuffix(req.URL.Path, "/plans")
+			return method && url && contentType && name && memory && swap && cpuShare && deflt && router
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := PlanCreate{}
+	command.Flags().Parse(true, []string{"-c", "100", "-m", "4194304", "-s", "512", "-d", "-r", "myrouter"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, "Plan successfully created!\n")
+}
+
+func (s *S) TestPlanCreateMemoryAndSwapUnits(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"myplan"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusCreated},
+		CondFunc: func(req *http.Request) bool {
+			name := req.FormValue("name") == "myplan"
+			memory := req.FormValue("memory") == "100M"
+			swap := req.FormValue("swap") == "512K"
+			cpuShare := req.FormValue("cpushare") == "100"
+			deflt := req.FormValue("default") == "true"
+			router := req.FormValue("router") == "myrouter"
+			method := req.Method == "POST"
+			contentType := req.Header.Get("Content-Type") == "application/x-www-form-urlencoded"
+			url := strings.HasSuffix(req.URL.Path, "/plans")
+			return method && url && contentType && name && memory && swap && cpuShare && deflt && router
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := PlanCreate{}
+	command.Flags().Parse(true, []string{"-c", "100", "-m", "100M", "-s", "512K", "-d", "-r", "myrouter"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, "Plan successfully created!\n")
+}
+
+func (s *S) TestPlanCreateError(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"myplan"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusConflict},
+		CondFunc: func(req *http.Request) bool {
+			return strings.HasSuffix(req.URL.Path, "/plans") && req.Method == "POST"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := PlanCreate{}
+	command.Flags().Parse(true, []string{"-c", "5"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.NotNil)
+	c.Assert(stdout.String(), check.Equals, "Failed to create plan!\n")
+}
+
+func (s *S) TestPlanCreateInvalidMemory(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"myplan"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusBadRequest},
+		CondFunc: func(req *http.Request) bool {
+			name := req.FormValue("name") == "myplan"
+			memory := req.FormValue("memory") == "4"
+			swap := req.FormValue("swap") == "0"
+			cpuShare := req.FormValue("cpushare") == "100"
+			deflt := req.FormValue("default") == "false"
+			router := req.FormValue("router") == ""
+			method := req.Method == "POST"
+			contentType := req.Header.Get("Content-Type") == "application/x-www-form-urlencoded"
+			url := strings.HasSuffix(req.URL.Path, "/plans")
+			return method && url && contentType && name && memory && swap && cpuShare && deflt && router
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := PlanCreate{}
+	command.Flags().Parse(true, []string{"-c", "100", "-m", "4"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.NotNil)
+	c.Assert(stdout.String(), check.Equals, "Failed to create plan!\n")
+}
+
+func (s *S) TestPlanCreateInvalidCpushare(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"myplan"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusBadRequest},
+		CondFunc: func(req *http.Request) bool {
+			name := req.FormValue("name") == "myplan"
+			memory := req.FormValue("memory") == "4194304"
+			swap := req.FormValue("swap") == "0"
+			cpuShare := req.FormValue("cpushare") == "1"
+			deflt := req.FormValue("default") == "false"
+			router := req.FormValue("router") == ""
+			method := req.Method == "POST"
+			contentType := req.Header.Get("Content-Type") == "application/x-www-form-urlencoded"
+			url := strings.HasSuffix(req.URL.Path, "/plans")
+			return method && url && contentType && name && memory && swap && cpuShare && deflt && router
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := PlanCreate{}
+	command.Flags().Parse(true, []string{"-c", "1", "-m", "4194304"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.NotNil)
+	c.Assert(stdout.String(), check.Equals, "Failed to create plan!\n")
+}
+
+func (s *S) TestPlanRemove(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"myplan"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			return strings.HasSuffix(req.URL.Path, "/plans/myplan") && req.Method == "DELETE"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := PlanRemove{}
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, "Plan successfully removed!\n")
+}
+
+func (s *S) TestPlanRemoveError(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"myplan"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusInternalServerError},
+		CondFunc: func(req *http.Request) bool {
+			return strings.HasSuffix(req.URL.Path, "/plans/myplan") && req.Method == "DELETE"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := PlanRemove{}
+	err := command.Run(&context, client)
+	c.Assert(err, check.NotNil)
+	c.Assert(stdout.String(), check.Equals, "Failed to remove plan!\n")
+}
+
+func (s *S) TestPlanRoutersListRun(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   nil,
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	r1 := router.PlanRouter{Name: "router1", Type: "foo"}
+	r2 := router.PlanRouter{Name: "router2", Type: "bar"}
+	data, err := json.Marshal([]router.PlanRouter{r1, r2})
+	c.Assert(err, check.IsNil)
+	expected := `+---------+------+
+| Name    | Type |
++---------+------+
+| router1 | foo  |
++---------+------+
+| router2 | bar  |
++---------+------+
+`
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: string(data), Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			return strings.HasSuffix(req.URL.Path, "/plans/routers") && req.Method == "GET"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := PlanRoutersList{}
+	err = command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, expected)
+}
