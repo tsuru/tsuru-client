d58160408a4c91996f33269064aa4b8191088212
André Carvalho <asantostc@gmail.com>
2017-03-13 15:32:57 -0300

    admin: add cmds to manage event blocks

---
+++ tsuru/admin/event.go
@@ -0,0 +1,184 @@
+// Copyright 2017 tsuru-client authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package admin
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/ajg/form"
+	"github.com/tsuru/gnuflag"
+	"github.com/tsuru/tsuru/cmd"
+	"github.com/tsuru/tsuru/event"
+)
+
+type EventBlockList struct {
+	fs     *gnuflag.FlagSet
+	active bool
+}
+
+func (c *EventBlockList) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "event-block-list",
+		Usage:   "event-block-list [-a/--active]",
+		Desc:    "Lists all event blocks",
+		MinArgs: 0,
+	}
+}
+
+func (c *EventBlockList) Flags() *gnuflag.FlagSet {
+	if c.fs == nil {
+		c.fs = gnuflag.NewFlagSet("", gnuflag.ExitOnError)
+		c.fs.BoolVar(&c.active, "active", false, "Display only active blocks.")
+		c.fs.BoolVar(&c.active, "a", false, "Display only active blocks.")
+	}
+	return c.fs
+}
+
+func (c *EventBlockList) Run(context *cmd.Context, client *cmd.Client) error {
+	path := "/events/blocks"
+	if c.active {
+		path += "?active=true"
+	}
+	url, err := cmd.GetURLVersion("1.3", path)
+	if err != nil {
+		return err
+	}
+	request, _ := http.NewRequest("GET", url, nil)
+	resp, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	var blocks []event.Block
+	err = json.NewDecoder(resp.Body).Decode(&blocks)
+	if err != nil {
+		return err
+	}
+	tbl := cmd.NewTable()
+	tbl.Headers = cmd.Row{"ID", "Start (duration)", "Kind", "Owner", "Target (Type: Value)", "Reason"}
+	for _, b := range blocks {
+		ts := b.StartTime.Format(time.RFC822Z)
+		if !b.EndTime.IsZero() {
+			ts = fmt.Sprintf("%s (%v)", ts, b.EndTime.Sub(b.StartTime))
+		}
+		kind := valueOrWildcard(b.KindName)
+		owner := valueOrWildcard(b.OwnerName)
+		targetType := valueOrWildcard(string(b.Target.Type))
+		targetValue := valueOrWildcard(b.Target.Value)
+		row := cmd.Row{b.ID.Hex(), ts, kind, owner, fmt.Sprintf("%s: %s", targetType, targetValue), b.Reason}
+		color := "yellow"
+		if !b.Active {
+			color = "white"
+		}
+		for i, v := range row {
+			row[i] = cmd.Colorfy(v, color, "", "")
+		}
+		tbl.AddRow(row)
+	}
+	context.Stdout.Write([]byte(tbl.String()))
+	return nil
+}
+
+func valueOrWildcard(str string) string {
+	if str == "" {
+		return "all"
+	}
+	return str
+}
+
+type EventBlockAdd struct {
+	fs          *gnuflag.FlagSet
+	kind        string
+	owner       string
+	targetType  string
+	targetValue string
+	reason      string
+}
+
+func (c *EventBlockAdd) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "event-block",
+		Usage:   "event-block <reason> [-k/--kind kindName] [-o/--owner ownerName] [-t/--target targetType] [-v/--value targetValue]",
+		Desc:    "Block events.",
+		MinArgs: 1,
+	}
+}
+
+func (c *EventBlockAdd) Flags() *gnuflag.FlagSet {
+	if c.fs == nil {
+		c.fs = gnuflag.NewFlagSet("", gnuflag.ExitOnError)
+		c.fs.StringVar(&c.kind, "kind", "", "Event kind to be blocked.")
+		c.fs.StringVar(&c.kind, "k", "", "Event kind to be blocked.")
+		c.fs.StringVar(&c.owner, "owner", "", "Block this owner's events.")
+		c.fs.StringVar(&c.owner, "o", "", "Block this owner's events.")
+		c.fs.StringVar(&c.targetType, "target", "", "Block events with this target type.")
+		c.fs.StringVar(&c.targetType, "t", "", "Block events with this target type.")
+		c.fs.StringVar(&c.targetValue, "value", "", "Block events with this target value.")
+		c.fs.StringVar(&c.targetValue, "v", "", "Block events with this target value.")
+	}
+	return c.fs
+}
+
+func (c *EventBlockAdd) Run(context *cmd.Context, client *cmd.Client) error {
+	url, err := cmd.GetURLVersion("1.3", "/events/blocks")
+	if err != nil {
+		return err
+	}
+	target := event.Target{}
+	if c.targetType != "" {
+		targetType, err := event.GetTargetType(c.targetType)
+		if err != nil {
+			return err
+		}
+		target.Type = targetType
+	}
+	target.Value = c.targetValue
+	block := event.Block{
+		Reason:    context.Args[0],
+		KindName:  c.kind,
+		OwnerName: c.owner,
+		Target:    target,
+	}
+	v, err := form.EncodeToValues(&block)
+	if err != nil {
+		return err
+	}
+	err = doRequest(client, url, http.MethodPost, v.Encode())
+	if err != nil {
+		return err
+	}
+	context.Stdout.Write([]byte("Block successfully added.\n"))
+	return nil
+}
+
+type EventBlockRemove struct{}
+
+func (c *EventBlockRemove) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "event-unblock",
+		Usage:   "event-block <ID>",
+		Desc:    "Unblock event.",
+		MinArgs: 1,
+		MaxArgs: 1,
+	}
+}
+
+func (c *EventBlockRemove) Run(context *cmd.Context, client *cmd.Client) error {
+	uuid := context.Args[0]
+	url, err := cmd.GetURLVersion("1.3", fmt.Sprintf("/events/blocks/%s", uuid))
+	if err != nil {
+		return err
+	}
+	request, _ := http.NewRequest(http.MethodDelete, url, nil)
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+	context.Stdout.Write([]byte(fmt.Sprintf("Block %s successfully removed.\n", uuid)))
+	return nil
+}
---
+++ tsuru/admin/event_test.go
@@ -0,0 +1,157 @@
+// Copyright 2017 tsuru-client authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package admin
+
+import (
+	"bytes"
+	"net/http"
+	"os"
+
+	"github.com/ajg/form"
+	"github.com/tsuru/tsuru/cmd"
+	"github.com/tsuru/tsuru/cmd/cmdtest"
+	"github.com/tsuru/tsuru/event"
+	check "gopkg.in/check.v1"
+)
+
+func (s *S) TestEventBlockList(c *check.C) {
+	os.Setenv("TSURU_DISABLE_COLORS", "1")
+	defer os.Unsetenv("TSURU_DISABLE_COLORS")
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	blocksData := `
+[{
+    "ID": "58c6db0b0640fd2fec413cc6",
+    "StartTime": "2017-03-13T17:46:51.326Z",
+    "EndTime": "0001-01-01T00:00:00Z",
+    "KindName": "app.create",
+    "OwnerName": "user@email.com",
+    "Target": {
+      "Type": "",
+      "Value": ""
+    },
+    "Reason": "Problems",
+    "Active": true
+  }, {
+	"ID": "58c1d29ac47369e95c5520c8",
+	"StartTime": "2017-03-13T16:43:09.888Z",
+	"EndTime": "2017-03-13T17:27:25.149Z",
+	"KindName": "app.deploy",
+	"OwnerName": "",
+	"Target": {
+		"Type": "",
+		"Value": ""
+	},
+	"Reason": "Maintenance.",
+	"Active": false
+}]`
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: blocksData, Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			return req.URL.Path == "/1.3/events/blocks"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := EventBlockList{}
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	expected := `+--------------------------+------------------------------------+------------+----------------+----------------------+--------------+
+| ID                       | Start (duration)                   | Kind       | Owner          | Target (Type: Value) | Reason       |
++--------------------------+------------------------------------+------------+----------------+----------------------+--------------+
+| 58c6db0b0640fd2fec413cc6 | 13 Mar 17 17:46 +0000              | app.create | user@email.com | all: all             | Problems     |
+| 58c1d29ac47369e95c5520c8 | 13 Mar 17 16:43 +0000 (44m15.261s) | app.deploy | all            | all: all             | Maintenance. |
++--------------------------+------------------------------------+------------+----------------+----------------------+--------------+
+`
+	c.Assert(stdout.String(), check.Equals, expected)
+}
+
+func (s *S) TestEventBlockAdd(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"Reason"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			var block event.Block
+			err := req.ParseForm()
+			c.Assert(err, check.IsNil)
+			dec := form.NewDecoder(nil)
+			dec.IgnoreUnknownKeys(true)
+			dec.IgnoreCase(true)
+			err = dec.DecodeValues(&block, req.Form)
+			c.Assert(err, check.IsNil)
+			c.Assert(block, check.DeepEquals, event.Block{Reason: "Reason", Active: false})
+			return req.URL.Path == "/1.3/events/blocks" && req.Method == http.MethodPost
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := EventBlockAdd{}
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, "Block successfully added.\n")
+}
+
+func (s *S) TestEventBlockAddAllFlags(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"Reason"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			var block event.Block
+			err := req.ParseForm()
+			c.Assert(err, check.IsNil)
+			dec := form.NewDecoder(nil)
+			dec.IgnoreUnknownKeys(true)
+			dec.IgnoreCase(true)
+			err = dec.DecodeValues(&block, req.Form)
+			c.Assert(err, check.IsNil)
+			c.Assert(block, check.DeepEquals, event.Block{
+				KindName:  "app.deploy",
+				OwnerName: "user@email.com",
+				Target:    event.Target{Type: event.TargetTypeApp, Value: "myapp"},
+				Reason:    "Reason",
+				Active:    false,
+			})
+			return req.URL.Path == "/1.3/events/blocks" && req.Method == http.MethodPost
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := EventBlockAdd{}
+	command.Flags().Parse(true, []string{"-k", "app.deploy", "-o", "user@email.com", "-t", "app", "-v", "myapp"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, "Block successfully added.\n")
+}
+
+func (s *S) TestEventBlockRemove(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"ABC123K12"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			return req.URL.Path == "/1.3/events/blocks/ABC123K12" && req.Method == http.MethodDelete
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, s.manager)
+	command := EventBlockRemove{}
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, "Block ABC123K12 successfully removed.\n")
+}
