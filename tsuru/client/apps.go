3f7df43fa93f20e827057daf29d0c3642102a8d0
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-03-16 10:43:18 -0300

    client: update app-list description

--- tsuru/client/apps.go
+++ tsuru/client/apps.go
@@ -792,7 +792,7 @@ func (c *AppList) Flags() *gnuflag.FlagSet {
 		c.fs.BoolVar(&c.filter.locked, "locked", false, "Filter applications by lock status")
 		c.fs.BoolVar(&c.filter.locked, "l", false, "Filter applications by lock status")
 		c.fs.BoolVar(&c.simplified, "q", false, "Display only applications name")
-		tagMessage := "Filter applications by tags"
+		tagMessage := "Filter applications by tag. Can be used multiple times"
 		c.fs.Var(&c.filter.tags, "tag", tagMessage)
 		c.fs.Var(&c.filter.tags, "g", tagMessage)
 	}
