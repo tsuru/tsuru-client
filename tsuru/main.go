3640a1a46509fc094f59865e75f1ec98fefafe08
Guilherme Paixão <guiferpa@gmail.com>
2017-08-15 17:07:05 -0300

    cmd: create cmd to list infos about team #1610

--- tsuru/main.go
+++ tsuru/main.go
@@ -82,6 +82,7 @@ func buildManager(name string) *cmd.Manager {
 	m.Register(&client.TeamUpdate{})
 	m.Register(&client.TeamRemove{})
 	m.Register(&client.TeamList{})
+	m.Register(&client.TeamInfo{})
 	m.Register(&client.ChangePassword{})
 	m.Register(&client.ShowAPIToken{})
 	m.Register(&client.RegenerateAPIToken{})
