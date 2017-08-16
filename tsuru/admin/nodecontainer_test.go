a8f03df7f4ae3efd69b6163db4f5c64e7573cd9d
Cezar Sa Espinola <cezarsa@gmail.com>
2017-08-16 19:02:22 -0300

    admin: allow enable and disable in node-container-add/update

--- tsuru/admin/nodecontainer_test.go
+++ tsuru/admin/nodecontainer_test.go
@@ -16,7 +16,7 @@ import (
 func (s *S) TestNodeContainerInfoRun(c *check.C) {
 	var buf bytes.Buffer
 	context := cmd.Context{Args: []string{"n1"}, Stdout: &buf}
-	body := `{"": {"config": {"image": "img1"}}, "p1": {"config": {"image": "img2"}}}`
+	body := `{"": {"config": {"image": "img1"}}, "p1": {"config": {"image": "img2"}}, "p2": {"disabled": false, "config": {"image": "img2"}}}`
 	trans := &cmdtest.ConditionalTransport{
 		Transport: cmdtest.Transport{Message: body, Status: http.StatusOK},
 		CondFunc: func(req *http.Request) bool {
@@ -34,6 +34,7 @@ func (s *S) TestNodeContainerInfoRun(c *check.C) {
 | <all> | {                        |
 |       |   "Name": "",            |
 |       |   "PinnedImage": "",     |
+|       |   "Disabled": null,      |
 |       |   "Config": {            |
 |       |     "Cmd": null,         |
 |       |     "Image": "img1",     |
@@ -48,6 +49,22 @@ func (s *S) TestNodeContainerInfoRun(c *check.C) {
 | p1    | {                        |
 |       |   "Name": "",            |
 |       |   "PinnedImage": "",     |
+|       |   "Disabled": null,      |
+|       |   "Config": {            |
+|       |     "Cmd": null,         |
+|       |     "Image": "img2",     |
+|       |     "Entrypoint": null   |
+|       |   },                     |
+|       |   "HostConfig": {        |
+|       |     "RestartPolicy": {}, |
+|       |     "LogConfig": {}      |
+|       |   }                      |
+|       | }                        |
++-------+--------------------------+
+| p2    | {                        |
+|       |   "Name": "",            |
+|       |   "PinnedImage": "",     |
+|       |   "Disabled": false,     |
 |       |   "Config": {            |
 |       |     "Cmd": null,         |
 |       |     "Image": "img2",     |
@@ -129,6 +146,7 @@ func (s *S) TestNodeContainerUpdateRun(c *check.C) {
 			c.Assert(err, check.IsNil)
 			_, isSet := req.Form["config.cmd"]
 			c.Assert(isSet, check.Equals, false)
+			c.Assert(req.FormValue("disabled"), check.Equals, "")
 			c.Assert(req.FormValue("config.cmd.0"), check.Equals, "echo")
 			c.Assert(req.FormValue("config.image"), check.Equals, "img2")
 			c.Assert(req.FormValue("hostconfig.binds.0"), check.Equals, "/a:/b")
@@ -144,6 +162,68 @@ func (s *S) TestNodeContainerUpdateRun(c *check.C) {
 	client := cmd.NewClient(&http.Client{Transport: trans}, nil, &manager)
 	command := NodeContainerUpdate{}
 	command.Flags().Parse(true, []string{"--image", "img1", "-p", "80:8080", "-v", "/a:/b", "-v", "/c:/d", "-r", "Config.image=img2", "-r", "config.cmd.0=echo", "--log-opt", "a=b"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(buf.String(), check.Equals, "Node container successfully updated.\n")
+}
+
+func (s *S) TestNodeContainerUpdateRunDisable(c *check.C) {
+	var buf bytes.Buffer
+	context := cmd.Context{Args: []string{"n1"}, Stdout: &buf}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			err := req.ParseForm()
+			c.Assert(err, check.IsNil)
+			_, isSet := req.Form["config.cmd"]
+			c.Assert(isSet, check.Equals, false)
+			c.Assert(req.FormValue("disabled"), check.Equals, "true")
+			c.Assert(req.FormValue("config.cmd.0"), check.Equals, "echo")
+			c.Assert(req.FormValue("config.image"), check.Equals, "img2")
+			c.Assert(req.FormValue("hostconfig.binds.0"), check.Equals, "/a:/b")
+			c.Assert(req.FormValue("hostconfig.binds.1"), check.Equals, "/c:/d")
+			c.Assert(req.FormValue("hostconfig.logconfig.config.a"), check.Equals, "b")
+			c.Assert(req.Form["config.exposedports.8080/tcp"], check.DeepEquals, []string{""})
+			c.Assert(req.Form["hostconfig.portbindings.8080/tcp.0.hostport"], check.DeepEquals, []string{"80"})
+			c.Assert(req.Form["hostconfig.portbindings.8080/tcp.0.hostip"], check.DeepEquals, []string{""})
+			return req.URL.Path == "/1.0/docker/nodecontainers/n1" && req.Method == "POST"
+		},
+	}
+	manager := cmd.Manager{}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, &manager)
+	command := NodeContainerUpdate{}
+	command.Flags().Parse(true, []string{"--disable", "--image", "img1", "-p", "80:8080", "-v", "/a:/b", "-v", "/c:/d", "-r", "Config.image=img2", "-r", "config.cmd.0=echo", "--log-opt", "a=b"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(buf.String(), check.Equals, "Node container successfully updated.\n")
+}
+
+func (s *S) TestNodeContainerUpdateRunEnable(c *check.C) {
+	var buf bytes.Buffer
+	context := cmd.Context{Args: []string{"n1"}, Stdout: &buf}
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			err := req.ParseForm()
+			c.Assert(err, check.IsNil)
+			_, isSet := req.Form["config.cmd"]
+			c.Assert(isSet, check.Equals, false)
+			c.Assert(req.FormValue("disabled"), check.Equals, "false")
+			c.Assert(req.FormValue("config.cmd.0"), check.Equals, "echo")
+			c.Assert(req.FormValue("config.image"), check.Equals, "img2")
+			c.Assert(req.FormValue("hostconfig.binds.0"), check.Equals, "/a:/b")
+			c.Assert(req.FormValue("hostconfig.binds.1"), check.Equals, "/c:/d")
+			c.Assert(req.FormValue("hostconfig.logconfig.config.a"), check.Equals, "b")
+			c.Assert(req.Form["config.exposedports.8080/tcp"], check.DeepEquals, []string{""})
+			c.Assert(req.Form["hostconfig.portbindings.8080/tcp.0.hostport"], check.DeepEquals, []string{"80"})
+			c.Assert(req.Form["hostconfig.portbindings.8080/tcp.0.hostip"], check.DeepEquals, []string{""})
+			return req.URL.Path == "/1.0/docker/nodecontainers/n1" && req.Method == "POST"
+		},
+	}
+	manager := cmd.Manager{}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, &manager)
+	command := NodeContainerUpdate{}
+	command.Flags().Parse(true, []string{"--enable", "--image", "img1", "-p", "80:8080", "-v", "/a:/b", "-v", "/c:/d", "-r", "Config.image=img2", "-r", "config.cmd.0=echo", "--log-opt", "a=b"})
 	err := command.Run(&context, client)
 	c.Assert(err, check.IsNil)
 	c.Assert(buf.String(), check.Equals, "Node container successfully updated.\n")
