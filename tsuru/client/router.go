4d47e7884f66a97dc20e45321dd99d240e6fdceb
Cezar Sa Espinola <cezarsa@gmail.com>
2017-09-19 14:07:08 -0300

    client: add commands to manipulate app routers
    Changes to app-info to include routers.
    app-router-list
    app-router-add
    app-router-remove

--- tsuru/client/router.go
+++ tsuru/client/router.go
@@ -6,10 +6,17 @@ package client
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"sort"
+	"strings"
 
+	"github.com/ajg/form"
+	"github.com/tsuru/gnuflag"
 	"github.com/tsuru/tsuru/cmd"
 	"github.com/tsuru/tsuru/router"
+	appTypes "github.com/tsuru/tsuru/types/app"
 )
 
 type RoutersList struct{}
@@ -50,5 +57,155 @@ func (c *RoutersList) Run(context *cmd.Context, client *cmd.Client) error {
 		table.AddRow(cmd.Row([]string{router.Name, router.Type}))
 	}
 	context.Stdout.Write(table.Bytes())
+	return nil
+}
+
+type AppRoutersList struct {
+	cmd.GuessingCommand
+}
+
+func (c *AppRoutersList) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "app-router-list",
+		Usage:   "app-router-list [-a/--app appname]",
+		Desc:    "List all routers associated to an application.",
+		MinArgs: 0,
+	}
+}
+
+func (c *AppRoutersList) Run(context *cmd.Context, client *cmd.Client) error {
+	appName, err := c.Guess()
+	if err != nil {
+		return err
+	}
+	url, err := cmd.GetURLVersion("1.5", fmt.Sprintf("/apps/%s/routers", appName))
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	if response.StatusCode == http.StatusNoContent {
+		fmt.Fprintln(context.Stdout, "No routers available for app.")
+		return nil
+	}
+	var routers []appTypes.AppRouter
+	err = json.NewDecoder(response.Body).Decode(&routers)
+	if err != nil {
+		return err
+	}
+	renderRouters(routers, context.Stdout)
+	return nil
+}
+
+func renderRouters(routers []appTypes.AppRouter, out io.Writer) {
+	table := cmd.NewTable()
+	table.Headers = cmd.Row([]string{"Name", "Opts", "Address"})
+	table.LineSeparator = true
+	for _, r := range routers {
+		var optsStr []string
+		for k, v := range r.Opts {
+			optsStr = append(optsStr, fmt.Sprintf("%s: %s", k, v))
+		}
+		sort.Strings(optsStr)
+		table.AddRow(cmd.Row([]string{r.Name, strings.Join(optsStr, "\n"), r.Address}))
+	}
+	out.Write(table.Bytes())
+}
+
+type AppRoutersAdd struct {
+	cmd.GuessingCommand
+	opts cmd.MapFlag
+	fs   *gnuflag.FlagSet
+}
+
+func (c *AppRoutersAdd) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "app-router-add",
+		Usage:   "app-router-add <router name> [-a/--app appname] [-o/--opts key=value]...",
+		Desc:    "Add a new router to an application.",
+		MinArgs: 1,
+		MaxArgs: 1,
+	}
+}
+
+func (c *AppRoutersAdd) Flags() *gnuflag.FlagSet {
+	if c.fs == nil {
+		c.fs = c.GuessingCommand.Flags()
+		optsMessage := "Custom options sent directly to router implementation."
+		c.fs.Var(&c.opts, "o", optsMessage)
+		c.fs.Var(&c.opts, "opts", optsMessage)
+	}
+	return c.fs
+}
+
+func (c *AppRoutersAdd) Run(context *cmd.Context, client *cmd.Client) error {
+	appName, err := c.Guess()
+	if err != nil {
+		return err
+	}
+	url, err := cmd.GetURLVersion("1.5", fmt.Sprintf("/apps/%s/routers", appName))
+	if err != nil {
+		return err
+	}
+	r := appTypes.AppRouter{
+		Name: context.Args[0],
+		Opts: c.opts,
+	}
+	val, err := form.EncodeToValues(r)
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest("POST", url, strings.NewReader(val.Encode()))
+	if err != nil {
+		return err
+	}
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(context.Stdout, "Router successfully added.")
+	return nil
+}
+
+type AppRoutersRemove struct {
+	cmd.GuessingCommand
+}
+
+func (c *AppRoutersRemove) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "app-router-remove",
+		Usage:   "app-router-remove <router name> [-a/--app appname]",
+		Desc:    "Remove a router from an application.",
+		MinArgs: 1,
+		MaxArgs: 1,
+	}
+}
+
+func (c *AppRoutersRemove) Run(context *cmd.Context, client *cmd.Client) error {
+	appName, err := c.Guess()
+	if err != nil {
+		return err
+	}
+	url, err := cmd.GetURLVersion("1.5", fmt.Sprintf("/apps/%s/routers?name=%s", appName, context.Args[0]))
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return err
+	}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(context.Stdout, "Router successfully removed.")
 	return nil
 }
--- tsuru/client/router_test.go
+++ tsuru/client/router_test.go
@@ -8,11 +8,13 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/tsuru/tsuru/cmd"
 	"github.com/tsuru/tsuru/cmd/cmdtest"
 	"github.com/tsuru/tsuru/router"
+	appTypes "github.com/tsuru/tsuru/types/app"
 	check "gopkg.in/check.v1"
 )
 
@@ -44,6 +46,121 @@ func (s *S) TestRoutersListRun(c *check.C) {
 	client := cmd.NewClient(&http.Client{Transport: trans}, nil, manager)
 	command := RoutersList{}
 	err = command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, expected)
+}
+
+func (s *S) TestAppRoutersListRun(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   nil,
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	routers := []appTypes.AppRouter{
+		{Name: "r1", Opts: map[string]string{"a": "b", "x": "y"}, Address: "addr1"},
+		{Name: "r2", Address: "addr2"},
+	}
+	data, err := json.Marshal(routers)
+	c.Assert(err, check.IsNil)
+	expected := `+------+------+---------+
+| Name | Opts | Address |
++------+------+---------+
+| r1   | a: b | addr1   |
+|      | x: y |         |
++------+------+---------+
+| r2   |      | addr2   |
++------+------+---------+
+`
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: string(data), Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			return strings.HasSuffix(req.URL.Path, "/1.5/apps/myapp/routers") && req.Method == "GET"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, manager)
+	command := AppRoutersList{}
+	command.Flags().Parse(true, []string{"-a", "myapp"})
+	err = command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, expected)
+}
+
+func (s *S) TestAppRoutersListRunEmpty(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   nil,
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	expected := "No routers available for app.\n"
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusNoContent},
+		CondFunc: func(req *http.Request) bool {
+			return strings.HasSuffix(req.URL.Path, "/1.5/apps/myapp/routers") && req.Method == "GET"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, manager)
+	command := AppRoutersList{}
+	command.Flags().Parse(true, []string{"-a", "myapp"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, expected)
+}
+
+func (s *S) TestAppRoutersAddRun(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"myrouter"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	expected := "Router successfully added.\n"
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			err := req.ParseForm()
+			c.Assert(err, check.IsNil)
+			c.Assert(req.Form, check.DeepEquals, url.Values{
+				"Opts.a":  []string{"b"},
+				"Opts.x":  []string{"y"},
+				"Address": []string{""},
+				"Name":    []string{"myrouter"},
+			})
+			return strings.HasSuffix(req.URL.Path, "/1.5/apps/myapp/routers") && req.Method == "POST"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, manager)
+	command := AppRoutersAdd{}
+	command.Flags().Parse(true, []string{"-a", "myapp", "-o", "a=b", "-o", "x=y"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, expected)
+}
+
+func (s *S) TestAppRoutersRemoveRun(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"myrouter"},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	expected := "Router successfully removed.\n"
+	trans := &cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(req *http.Request) bool {
+			err := req.ParseForm()
+			c.Assert(err, check.IsNil)
+			c.Assert(req.Form, check.DeepEquals, url.Values{
+				"name": []string{"myrouter"},
+			})
+			return strings.HasSuffix(req.URL.Path, "/1.5/apps/myapp/routers") && req.Method == "DELETE"
+		},
+	}
+	client := cmd.NewClient(&http.Client{Transport: trans}, nil, manager)
+	command := AppRoutersRemove{}
+	command.Flags().Parse(true, []string{"-a", "myapp"})
+	err := command.Run(&context, client)
 	c.Assert(err, check.IsNil)
 	c.Assert(stdout.String(), check.Equals, expected)
 }
