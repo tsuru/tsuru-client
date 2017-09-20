337033615645294b9157ff2611a3ac8d9b0fe4f1
Arthur Silva <arxdsilva@gmail.com>
2017-09-20 10:26:34 -0300

    client/init_test: test to copyGitIgnore
        also changes test to use tmp/ instead of a custom path

--- tsuru/client/init_test.go
+++ tsuru/client/init_test.go
@@ -1,52 +1,78 @@
 package client
 
 import (
-	"bytes"
 	"errors"
-	"net/http"
+	"io/ioutil"
 	"os"
 	"path/filepath"
-
-	"github.com/tsuru/tsuru/cmd"
 
 	check "gopkg.in/check.v1"
 )
 
-func (s *S) TestInitCreatesFiles(c *check.C) {
+func (s *S) TestInitCreateInitFiles(c *check.C) {
 	wd, err := os.Getwd()
 	c.Assert(err, check.IsNil)
-	testPath := "testdata/deploy"
-	err = os.Chdir(filepath.Join(wd, testPath))
-	c.Assert(err, check.IsNil)
 	defer os.Chdir(wd)
-	var stdout bytes.Buffer
-	context := cmd.Context{Stdout: &stdout}
-	client := cmd.NewClient(&http.Client{}, nil, manager)
-	cmd := Init{}
-	err = cmd.Run(&context, client)
+	tmpDir := os.TempDir()
+	err = os.Chdir(tmpDir)
 	c.Assert(err, check.IsNil)
-	tpath, err := os.Open(filepath.Join(wd, testPath))
+	err = createInitFiles()
+	c.Assert(err, check.IsNil)
+	tpath, err := os.Open(tmpDir)
 	c.Assert(err, check.IsNil)
 	defer tpath.Close()
 	content, err := tpath.Readdir(0)
 	c.Assert(err, check.IsNil)
-	var addedFiles []string
+	var createdFiles []string
 	for _, c := range content {
 		if (c.Name() == ".tsuruignore") || (c.Name() == "Procfile") || (c.Name() == "tsuru.yaml") {
-			addedFiles = append(addedFiles, c.Name())
+			createdFiles = append(createdFiles, c.Name())
 		}
 	}
-	for _, c := range addedFiles {
-		err = os.Remove(c)
+	for _, cf := range createdFiles {
+		err = os.Remove(cf)
+		c.Assert(err, check.IsNil)
 	}
-	c.Assert(err, check.IsNil)
-	if len(addedFiles) != 3 {
+	if len(createdFiles) != 3 {
 		err = errors.New("Tsuru init failed to create a file")
 	}
 	c.Assert(err, check.IsNil)
-	c.Assert(stdout.String(), check.NotNil)
 }
 
 func (s *S) TestInitInfo(c *check.C) {
 	c.Assert((&Init{}).Info(), check.NotNil)
+}
+
+func (s *S) TestCopyGitIgnoreWithGitIgnore(c *check.C) {
+	expected := "vendor/\n.git\n.gitignore\n"
+	wd, err := os.Getwd()
+	c.Assert(err, check.IsNil)
+	testPath := "testdata/deploy3"
+	err = os.Chdir(filepath.Join(wd, testPath))
+	c.Assert(err, check.IsNil)
+	defer os.Chdir(wd)
+	err = copyGitIgnore()
+	c.Assert(err, check.IsNil)
+	tsuruignore, err := ioutil.ReadFile(".tsuruignore")
+	c.Assert(err, check.IsNil)
+	c.Assert(string(tsuruignore), check.Equals, expected)
+	err = ioutil.WriteFile(".tsuruignore", []byte(""), 0644)
+	c.Assert(err, check.IsNil)
+}
+
+func (s *S) TestCopyGitIgnoreWithoutGitIgnore(c *check.C) {
+	expected := ".git\n.gitignore\n"
+	wd, err := os.Getwd()
+	c.Assert(err, check.IsNil)
+	tmpDir := os.TempDir()
+	err = os.Chdir(tmpDir)
+	c.Assert(err, check.IsNil)
+	defer os.Chdir(wd)
+	err = copyGitIgnore()
+	c.Assert(err, check.IsNil)
+	tsuruignore, err := ioutil.ReadFile(".tsuruignore")
+	c.Assert(err, check.IsNil)
+	c.Assert(string(tsuruignore), check.Equals, expected)
+	err = os.Remove(filepath.Join(tmpDir, ".tsuruignore"))
+	c.Assert(err, check.IsNil)
 }
