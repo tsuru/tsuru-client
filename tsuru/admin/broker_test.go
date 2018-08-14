446b107873d56a334c6d3ae5c02579b4ec3eec57
André Carvalho <asantostc@gmail.com>
2018-08-14 15:22:14 -0300

    admin: sets bearerconfig and basicauthconfig to nil if fields are not set

--- tsuru/admin/broker_test.go
+++ tsuru/admin/broker_test.go
@@ -62,6 +62,51 @@ func (s *S) TestBrokerAdd(c *check.C) {
 	c.Assert(stdout.String(), check.Equals, expected)
 }
 
+func (s *S) TestBrokerAddEmptyAuth(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	expected := "Service broker successfully added.\n"
+	context := cmd.Context{
+		Stdout: &stdout,
+		Stderr: &stderr,
+		Args:   []string{"br1", "http://x.com"},
+	}
+	trans := cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(r *http.Request) bool {
+			c.Assert(r.URL.Path, check.Equals, "/1.7/brokers")
+			c.Assert(r.Method, check.Equals, "POST")
+			var ret tsuru.ServiceBroker
+			c.Assert(r.Header.Get("Content-Type"), check.Equals, "application/json")
+			data, err := ioutil.ReadAll(r.Body)
+			c.Assert(err, check.IsNil)
+			err = json.Unmarshal(data, &ret)
+			c.Assert(err, check.IsNil)
+			c.Assert(ret, check.DeepEquals, tsuru.ServiceBroker{
+				Name: "br1",
+				URL:  "http://x.com",
+				Config: &tsuru.ServiceBrokerConfig{
+					AuthConfig: &tsuru.ServiceBrokerConfigAuthConfig{
+						BasicAuthConfig: nil,
+						BearerConfig:    nil,
+					},
+					Context: map[string]string{
+						"p1": "v1",
+						"p2": "v2",
+					},
+				},
+			})
+			return true
+		},
+	}
+	manager := cmd.NewManager("admin", "0.1", "admin-ver", &stdout, &stderr, nil, nil)
+	client := cmd.NewClient(&http.Client{Transport: &trans}, nil, manager)
+	command := BrokerAdd{}
+	command.Flags().Parse(true, []string{"-c", "p1=v1", "-c", "p2=v2"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, expected)
+}
+
 func (s *S) TestBrokerUpdate(c *check.C) {
 	var stdout, stderr bytes.Buffer
 	expected := "Service broker successfully updated.\n"
@@ -107,6 +152,51 @@ func (s *S) TestBrokerUpdate(c *check.C) {
 	client := cmd.NewClient(&http.Client{Transport: &trans}, nil, manager)
 	command := BrokerUpdate{}
 	command.Flags().Parse(true, []string{"-t", "ABCDE", "-p", "password", "-u", "username", "-c", "p1=v1", "-c", "p2=v2"})
+	err := command.Run(&context, client)
+	c.Assert(err, check.IsNil)
+	c.Assert(stdout.String(), check.Equals, expected)
+}
+
+func (s *S) TestBrokerUpdateEmptyAuth(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	expected := "Service broker successfully updated.\n"
+	context := cmd.Context{
+		Stdout: &stdout,
+		Stderr: &stderr,
+		Args:   []string{"br1", "http://x.com"},
+	}
+	trans := cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: "", Status: http.StatusOK},
+		CondFunc: func(r *http.Request) bool {
+			c.Assert(r.URL.Path, check.Equals, "/1.7/brokers/br1")
+			c.Assert(r.Method, check.Equals, "PUT")
+			var ret tsuru.ServiceBroker
+			c.Assert(r.Header.Get("Content-Type"), check.Equals, "application/json")
+			data, err := ioutil.ReadAll(r.Body)
+			c.Assert(err, check.IsNil)
+			err = json.Unmarshal(data, &ret)
+			c.Assert(err, check.IsNil)
+			c.Assert(ret, check.DeepEquals, tsuru.ServiceBroker{
+				Name: "br1",
+				URL:  "http://x.com",
+				Config: &tsuru.ServiceBrokerConfig{
+					AuthConfig: &tsuru.ServiceBrokerConfigAuthConfig{
+						BasicAuthConfig: nil,
+						BearerConfig:    nil,
+					},
+					Context: map[string]string{
+						"p1": "v1",
+						"p2": "v2",
+					},
+				},
+			})
+			return true
+		},
+	}
+	manager := cmd.NewManager("admin", "0.1", "admin-ver", &stdout, &stderr, nil, nil)
+	client := cmd.NewClient(&http.Client{Transport: &trans}, nil, manager)
+	command := BrokerUpdate{}
+	command.Flags().Parse(true, []string{"-c", "p1=v1", "-c", "p2=v2"})
 	err := command.Run(&context, client)
 	c.Assert(err, check.IsNil)
 	c.Assert(stdout.String(), check.Equals, expected)
