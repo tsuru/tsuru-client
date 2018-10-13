4252314e8330a34435cc2722eb5a9c0f250f1bf1
Rodrigo Brito <rig.10@hotmail.com>
2018-10-13 16:32:03 -0300

    Create test file for installer composer

---
+++ tsuru/installer/compose_test.go
@@ -0,0 +1,37 @@
+// Copyright 2016 tsuru-client authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package installer
+
+import (
+	"strings"
+
+	"github.com/tsuru/tsuru-client/tsuru/installer/defaultconfig"
+	"gopkg.in/check.v1"
+)
+
+func (s *S) TestResolverConfig(c *check.C) {
+	tt := []struct {
+		Description string
+		Base        string
+		Config      map[string]string
+		Result      string
+		ErrMsg      string
+	}{
+		{"invalid path", "invalid", nil, "", "no such file or directory"},
+		{"default configuration", "", nil, defaultconfig.Compose, ""},
+		{"custom parameter", "", map[string]string{"TSURU_API_IMAGE": "INJECT_TEST"}, "INJECT_TEST", ""},
+	}
+
+	for _, tc := range tt {
+		result, err := resolveConfig(tc.Base, tc.Config)
+		if len(tc.ErrMsg) > 0 {
+			contains := strings.Contains(err.Error(), tc.ErrMsg)
+			c.Assert(contains, check.Equals, true)
+		} else {
+			c.Assert(err, check.IsNil)
+		}
+		c.Assert(strings.Contains(result, tc.Result), check.Equals, true)
+	}
+}
