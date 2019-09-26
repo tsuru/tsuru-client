d8942bead7bc08c20f7c2a00308d2d5454c672d7
Claudio Netto <nettinhorama@gmail.com>
2019-09-25 22:12:16 -0300

    refactor(admin/cluster): increase error handling in cluster update

--- tsuru/admin/cluster.go
+++ tsuru/admin/cluster.go
@@ -125,6 +125,7 @@ type ClusterUpdate struct {
 	cacert           string
 	clientcert       string
 	clientkey        string
+	isDefault        string
 	removeCacert     bool
 	removeClientcert bool
 	removeClientkey  bool
@@ -132,10 +133,9 @@ type ClusterUpdate struct {
 	addPool          cmd.StringSliceFlag
 	removePool       cmd.StringSliceFlag
 	addCustomData    cmd.MapFlag
-	removeCustomData cmd.MapFlag
+	removeCustomData cmd.StringSliceFlag
 	addCreateData    cmd.MapFlag
-	removeCreateData cmd.MapFlag
-	isDefault        bool
+	removeCreateData cmd.StringSliceFlag
 }
 
 func (c *ClusterUpdate) Flags() *gnuflag.FlagSet {
@@ -154,7 +154,7 @@ func (c *ClusterUpdate) Flags() *gnuflag.FlagSet {
 		desc = "Remove path to client key file."
 		c.fs.BoolVar(&c.removeClientkey, "remove-clientkey", false, desc)
 		desc = "Whether this is the default cluster."
-		c.fs.BoolVar(&c.isDefault, "default", false, desc)
+		c.fs.StringVar(&c.isDefault, "default", "", desc)
 		desc = "Address to be used in cluster."
 		c.fs.Var(&c.addresses, "addr", desc)
 		desc = "Add pool which will use this cluster."
@@ -176,7 +176,7 @@ func (c *ClusterUpdate) Flags() *gnuflag.FlagSet {
 func (c *ClusterUpdate) Info() *cmd.Info {
 	return &cmd.Info{
 		Name:    "cluster-update",
-		Usage:   "cluster-update <name> <provisioner> --addr address... [--add-pool poolname] [--remove-pool poolname] ... [--cacert cacertfile] [--remove-cacert] [--clientcert clientcertfile] [--remove-clientcert] [--clientkey clientkeyfile] [--remove-clientkey] [--add-custom key=value] [--remove-custom key=value]... [--add-create-data key=value] [--remove-create-data key=value]... [--default]",
+		Usage:   "cluster-update <name> <provisioner> [--addr address]... [--add-pool poolname]... [--remove-pool poolname]... [--cacert cacertfile] [--remove-cacert] [--clientcert clientcertfile] [--remove-clientcert] [--clientkey clientkeyfile] [--remove-clientkey] [--add-custom key=value]... [--remove-custom key]... [--add-create-data key=value]... [--remove-create-data key]... [--default=true|false]",
 		Desc:    `Updates a provisioner cluster definition.`,
 		MinArgs: 2,
 		MaxArgs: 2,
@@ -192,16 +192,14 @@ func (c *ClusterUpdate) Run(ctx *cmd.Context, cli *cmd.Client) error {
 		return err
 	}
 	name := ctx.Args[0]
-	oldCluster, _, err := apiClient.ClusterApi.ClusterInfo(context.TODO(), name)
+	cluster, _, err := apiClient.ClusterApi.ClusterInfo(context.TODO(), name)
 	if err != nil {
 		return err
 	}
-
-	clus, err := c.mergeCluster(oldCluster)
-	if err != nil {
+	if err := c.mergeCluster(&cluster); err != nil {
 		return err
 	}
-	_, err = apiClient.ClusterApi.ClusterUpdate(context.TODO(), name, clus)
+	_, err = apiClient.ClusterApi.ClusterUpdate(context.TODO(), name, cluster)
 	if err != nil {
 		return err
 	}
@@ -209,90 +207,164 @@ func (c *ClusterUpdate) Run(ctx *cmd.Context, cli *cmd.Client) error {
 	return nil
 }
 
-func (c *ClusterUpdate) mergeCluster(oldCluster tsuru.Cluster) (tsuru.Cluster, error) {
+func (c *ClusterUpdate) mergeCluster(cluster *tsuru.Cluster) error {
+	if cluster == nil {
+		return fmt.Errorf("cannot update a nil cluster")
+	}
 	if c.addresses != nil {
-		oldCluster.Addresses = c.addresses
+		cluster.Addresses = c.addresses
+	}
+	if err := c.updateCACertificate(cluster); err != nil {
+		return err
+	}
+	if err := c.updateClientCertificate(cluster); err != nil {
+		return err
+	}
+	if err := c.updateClientKey(cluster); err != nil {
+		return err
+	}
+	if err := c.updateCreateData(cluster); err != nil {
+		return err
+	}
+	if err := c.updateCustomData(cluster); err != nil {
+		return err
+	}
+	if err := c.updatePools(cluster); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *ClusterUpdate) updateCACertificate(cluster *tsuru.Cluster) error {
+	if cluster == nil {
+		return fmt.Errorf("cannot update a nil cluster")
+	}
+	if c.removeCacert && c.cacert != "" {
+		return fmt.Errorf("cannot both remove and replace the CA certificate")
 	}
 	if c.removeCacert {
-		oldCluster.Cacert = nil
+		cluster.Cacert = nil
 	}
 	if c.cacert != "" {
 		data, err := ioutil.ReadFile(c.cacert)
 		if err != nil {
-			return oldCluster, err
+			return errors.Wrapf(err, "unable to read the CA certificate file")
 		}
-		oldCluster.Cacert = data
+		cluster.Cacert = data
+	}
+	return nil
+}
+
+func (c *ClusterUpdate) updateClientCertificate(cluster *tsuru.Cluster) error {
+	if cluster == nil {
+		return fmt.Errorf("cannot update a nil cluster")
+	}
+	if c.removeClientcert && c.clientcert != "" {
+		return fmt.Errorf("cannot both remove and replace the client certificate")
 	}
 	if c.removeClientcert {
-		oldCluster.Clientcert = nil
+		cluster.Clientcert = nil
 	}
 	if c.clientcert != "" {
 		data, err := ioutil.ReadFile(c.clientcert)
 		if err != nil {
-			return oldCluster, err
+			return errors.Wrapf(err, "unable to read the client certificate file")
 		}
-		oldCluster.Clientcert = data
+		cluster.Clientcert = data
+	}
+	return nil
+}
+
+func (c *ClusterUpdate) updateClientKey(cluster *tsuru.Cluster) error {
+	if cluster == nil {
+		return fmt.Errorf("cannot update a nil cluster")
+	}
+	if c.removeClientkey && c.clientkey != "" {
+		return fmt.Errorf("cannot both remove and replace the client key")
 	}
 	if c.removeClientkey {
-		oldCluster.Clientkey = nil
+		cluster.Clientkey = nil
 	}
 	if c.clientkey != "" {
 		data, err := ioutil.ReadFile(c.clientkey)
 		if err != nil {
-			return oldCluster, err
+			return errors.Wrapf(err, "unable to read the client key file")
 		}
-		oldCluster.Clientkey = data
+		cluster.Clientkey = data
 	}
-	if c.addCustomData != nil {
-		if oldCluster.CustomData == nil {
-			oldCluster.CustomData = make(map[string]string)
-		}
-		for key, value := range c.addCustomData {
-			oldCluster.CustomData[key] = value
-		}
+	return nil
+}
+
+func (c *ClusterUpdate) updateCustomData(cluster *tsuru.Cluster) error {
+	if cluster == nil {
+		return fmt.Errorf("cannot update a nil cluster")
 	}
-	if c.removeCustomData != nil {
-		for key := range c.removeCustomData {
-			delete(oldCluster.CustomData, key)
-		}
+	if cluster.CustomData == nil {
+		cluster.CustomData = make(map[string]string)
 	}
-	if c.addCreateData != nil {
-		if oldCluster.CreateData == nil {
-			oldCluster.CreateData = make(map[string]string)
-		}
-		for key, value := range c.addCreateData {
-			oldCluster.CreateData[key] = value
-		}
+	for key, value := range c.addCustomData {
+		cluster.CustomData[key] = value
 	}
-	if c.removeCreateData != nil {
-		for key := range c.removeCreateData {
-			delete(oldCluster.CreateData, key)
+	for _, key := range c.removeCustomData {
+		if _, hasKey := cluster.CustomData[key]; !hasKey {
+			return fmt.Errorf("cannot unset custom data entry: key %q not found", key)
 		}
+		delete(cluster.CustomData, key)
 	}
-	if !c.isDefault {
-		oldCluster.Default = false
-		if c.addPool != nil {
-			for _, pool := range c.addPool {
-				if hasPool(oldCluster.Pools, pool) {
-					continue
-				}
-				oldCluster.Pools = append(oldCluster.Pools, pool)
-			}
-		}
-		if c.removePool != nil {
-			for _, pool := range c.removePool {
-				for i, oldPool := range oldCluster.Pools {
-					if pool == oldPool {
-						oldCluster.Pools = append(oldCluster.Pools[:i], oldCluster.Pools[i+1:]...)
-					}
-				}
-			}
-		}
-	} else {
-		oldCluster.Pools = nil
-		oldCluster.Default = true
+	return nil
+}
+
+func (c *ClusterUpdate) updateCreateData(cluster *tsuru.Cluster) error {
+	if cluster == nil {
+		return fmt.Errorf("cannot update a nil cluster")
 	}
-	return oldCluster, nil
+	if cluster.CreateData == nil {
+		cluster.CreateData = make(map[string]string)
+	}
+	for key, value := range c.addCreateData {
+		cluster.CreateData[key] = value
+	}
+	for _, key := range c.removeCreateData {
+		if _, hasKey := cluster.CreateData[key]; !hasKey {
+			return fmt.Errorf("cannot unset create data entry: key %q not found", key)
+		}
+		delete(cluster.CreateData, key)
+	}
+	return nil
+}
+
+func (c *ClusterUpdate) updatePools(cluster *tsuru.Cluster) error {
+	if cluster == nil {
+		return fmt.Errorf("cannot update a nil cluster")
+	}
+	isDefault, err := strconv.ParseBool(c.isDefault)
+	if err != nil && c.isDefault != "" {
+		return errors.Wrapf(err, "unable to parse default option")
+	}
+	if err == nil {
+		cluster.Default = isDefault
+	}
+	if cluster.Default && (len(c.addPool) > 0 || len(c.removePool) > 0) {
+		return fmt.Errorf("cannot add or remove pools in a default cluster")
+	}
+	if cluster.Default {
+		cluster.Pools = make([]string, 0)
+		return nil
+	}
+	for _, pool := range c.addPool {
+		if _, ok := hasPool(cluster.Pools, pool); ok {
+			return fmt.Errorf("pool %q already defined", pool)
+		}
+		cluster.Pools = append(cluster.Pools, pool)
+	}
+	for _, pool := range c.removePool {
+		index, ok := hasPool(cluster.Pools, pool)
+		if !ok {
+			return fmt.Errorf("pool %q not found", pool)
+		}
+		cluster.Pools = append(cluster.Pools[:index], cluster.Pools[index+1:]...)
+	}
+	return nil
 }
 
 type ClusterList struct{}
@@ -434,7 +506,7 @@ func (c *ProvisionerInfo) Run(ctx *cmd.Context, cli *cmd.Client) error {
 	}
 	provisioners, resp, err := apiClient.ClusterApi.ProvisionerList(context.TODO())
 	if resp != nil && resp.StatusCode == http.StatusNoContent {
-		return errors.New("provisioner not found")
+		return fmt.Errorf("provisioner not found")
 	}
 	if err != nil {
 		return err
@@ -448,7 +520,7 @@ func (c *ProvisionerInfo) Run(ctx *cmd.Context, cli *cmd.Client) error {
 		}
 	}
 	if provisioner == nil {
-		return errors.New("provisioner not found")
+		return fmt.Errorf("provisioner not found")
 	}
 	fmt.Fprintf(ctx.Stdout, "Name: %v\n", provisioner.Name)
 	fmt.Fprintf(ctx.Stdout, "Cluster usage: %v\n", provisioner.ClusterHelp.ProvisionerHelp)
@@ -481,12 +553,11 @@ func optionalStreamResponse(w io.Writer, resp *http.Response) error {
 	return nil
 }
 
-func hasPool(oldPools []string, pool string) bool {
-
-	for _, oldPool := range oldPools {
-		if pool == oldPool {
-			return true
+func hasPool(pools []string, pool string) (int, bool) {
+	for i, p := range pools {
+		if pool == p {
+			return i, true
 		}
 	}
-	return false
+	return -1, false
 }
