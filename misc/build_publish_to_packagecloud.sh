921889c819388753cad65e4c000a1d804f480280
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-09-28 17:47:09 -0300

    publish current supported distro/version + any/any

--- misc/build_publish_to_packagecloud.sh
+++ misc/build_publish_to_packagecloud.sh
@@ -82,34 +82,37 @@ _build_all_packages(){
 }
 
 _publish_all_packages(){
-  # package cloud accepts numbers on some distros, althouth not documented
-  # DEB_DISTROS="any/any" # this breaks old repos :(
   DEB_DISTROS="
+any/any
+debian/jessie
 debian/stretch
 debian/buster
 debian/bullseye
 
-ubuntu/14.04
-ubuntu/16.04
-ubuntu/18.04
-ubuntu/20.04
-ubuntu/20.10
-ubuntu/21.04
-ubuntu/21.10
-ubuntu/22.04
+linuxmint/sarah
+linuxmint/serena
+linuxmint/sonya
+linuxmint/sylvia
+linuxmint/tara
+linuxmint/tessa
+linuxmint/tina
+linuxmint/tricia
+linuxmint/ulyana
 
-linuxmint/19
-linuxmint/19.1
-linuxmint/19.2
-linuxmint/19.3
-linuxmint/20
-linuxmint/20.1
-linuxmint/20.2
-linuxmint/20.3
+ubuntu/bionic
+ubuntu/focal
+ubuntu/jammy
+ubuntu/trusty
+ubuntu/xenial
+ubuntu/zesty
 "
   while read -r PACKAGE_FILE; do
     for DEB_DISTRO in $DEB_DISTROS; do
       [ "${DEB_DISTRO}" = "" ] && continue
+
+      # XXX: Getting ready for supporting any/any publishing. Publish non-amd64 for any/any only
+      [[ ! "${PACKAGE_FILE}" =~ "amd64" ]] && [ "${DEB_DISTRO}" != "any/any" ] && continue
+
       echo "Pushing ${PACKAGE_FILE} to packagecloud (${DEB_DISTRO})..."
       set +e
       if ! package_cloud push "${PACKAGECLOUD_REPO}/${DEB_DISTRO}" "${PACKAGE_FILE}" ; then
@@ -120,19 +123,14 @@ linuxmint/20.3
     done
   done < <(find dist -type f -name "*.deb")
 
-  # RPM_DISTROS="rpm_any/rpm_any" # this breaks old repos :(
   RPM_DISTROS="
+rpm_any/rpm_any
 el/6
 el/7
-el/8
-el/9
 
 fedora/31
 fedora/32
 fedora/33
-fedora/34
-fedora/35
-fedora/36
 "
   while read -r PACKAGE_FILE; do
     for RPM_DISTRO in $RPM_DISTROS; do
