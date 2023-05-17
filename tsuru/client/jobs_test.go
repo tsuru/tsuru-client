324e688519a44c48c46649a6b311157d2f8cf1b1
Arthur Cavalcante Gomes Coelho <arthur.cavalcante.puc@gmail.com>
2023-05-17 15:19:08 -0300

    chore: increase jobcreate test coverage

--- tsuru/client/jobs_test.go
+++ tsuru/client/jobs_test.go
@@ -3,7 +3,6 @@ package client
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -20,21 +19,16 @@ func (s *S) TestJobCreate(c *check.C) {
 		Stdout: &stdout,
 		Stderr: &stderr,
 	}
-	type ret struct {
-		Name 	string 	`json:"jobName"`
-		Status 	string 	`json:"status"`
-	}
 	expected := "Job \"loucoAbreu\" has been created!\nUse job info to check the status of the job.\n"
 	trans := cmdtest.ConditionalTransport{
 		Transport: cmdtest.Transport{Message: `{"jobName":"loucoAbreu","status":"success"}`, Status: http.StatusCreated},
 		CondFunc: func(r *http.Request) bool {
 			c.Assert(r.URL.Path, check.Equals, "/1.13/jobs")
 			c.Assert(r.Method, check.Equals, "POST")
-			var rr tsuru.InputJob
 			c.Assert(r.Header.Get("Content-Type"), check.Equals, "application/json")
 			data, err := ioutil.ReadAll(r.Body)
 			c.Assert(err, check.IsNil)
-			fmt.Printf("debug: 	%s\n", string(data))
+			var rr tsuru.InputJob
 			err = json.Unmarshal(data, &rr)
 			c.Assert(err, check.IsNil)
 			c.Assert(rr, check.DeepEquals, tsuru.InputJob{
@@ -57,4 +51,24 @@ func (s *S) TestJobCreate(c *check.C) {
 	err := command.Run(&context, client)
 	c.Assert(err, check.IsNil)
 	c.Assert(stdout.String(), check.Equals, expected)
+}
+
+func (s *S) TestJobCreateApiError(c *check.C) {
+	var stdout, stderr bytes.Buffer
+	context := cmd.Context{
+		Args:   []string{"loucoAbreu", "ubuntu:latest", "echo \"vivo essa paixão\""},
+		Stdout: &stdout,
+		Stderr: &stderr,
+	}
+	expected := "500 Internal Server Error: some error occcured"
+	trans := cmdtest.ConditionalTransport{
+		Transport: cmdtest.Transport{Message: `some error occcured`, Status: http.StatusInternalServerError},
+		CondFunc: func(r *http.Request) bool { return true },
+	}
+	client := cmd.NewClient(&http.Client{Transport: &trans}, nil, manager)
+	command := JobCreate{}
+	command.Info()
+	command.Flags().Parse(true, []string{"-t", "admin", "-o", "somepool", "-s", "* * * * *"})
+	err := command.Run(&context, client)
+	c.Assert(err.Error(), check.Equals, expected)
 }
