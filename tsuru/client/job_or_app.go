157ce26f1e675801fe674e2f4109270c9bae6a70
Wilson Júnior <wilsonpjunior@gmail.com>
2023-11-01 17:03:15 -0300

    Add logic to display metadata by process

--- tsuru/client/job_or_app.go
+++ tsuru/client/job_or_app.go
@@ -11,23 +11,34 @@ import (
 )
 
 type JobOrApp struct {
-	Type string
-	val  string
-	fs   *gnuflag.FlagSet
+	Type       string
+	val        string
+	appProcess string
+	fs         *gnuflag.FlagSet
 }
 
 func (c *JobOrApp) validate() error {
 	appName := c.fs.Lookup("app").Value.String()
 	jobName := c.fs.Lookup("job").Value.String()
+	var processName string
+
+	if flag := c.fs.Lookup("process"); flag != nil {
+		processName = flag.Value.String()
+	}
+
 	if appName == "" && jobName == "" {
 		return errors.New("job name or app name is required")
 	}
 	if appName != "" && jobName != "" {
 		return errors.New("please use only one of the -a/--app and -j/--job flags")
 	}
+	if processName != "" && jobName != "" {
+		return errors.New("please specify process just for an app")
+	}
 	if appName != "" {
 		c.Type = "app"
 		c.val = appName
+		c.appProcess = processName
 		return nil
 	}
 	c.Type = "job"
